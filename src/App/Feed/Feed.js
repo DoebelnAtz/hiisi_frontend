24d16f199c0d14215eca0b74055cf994a3f832f7
aadlercr <axel.ej.adlercreutz@gmail.com>
2019-12-28 21:28:02 +0200

    Feed animation update

--- src/App/Feed/Feed.js
+++ src/App/Feed/Feed.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Post from './Post/Post'
 import { makeRequest } from '../Api/Api'
-
+import {useSprings, useTransition, useTrail, animated} from 'react-spring'
 import CreatePostPopup from './CreatePostPopup'
 import Button from "../Components/Buttons/Button";
 
@@ -26,26 +26,30 @@ const Feed = () => {
         getPosts();
     }, []);
 
-    const renderList = () => {
-        if (posts.length) {
-            return (
-                posts.map((post) => {
-                    return (
-                        <div id={'feed'} key={post.id}>
-                            <Post content={post}/>
-                        </div>
-                    )
-                })
-            )
-        }
-        else{
-            return(
-                <div>
+    const config = { mass: 5, tension: 2000, friction: 200 };
 
-                </div>
-            )
-        }
+    const trail = useTrail(posts.length, {
+        config,
+        opacity: posts.length ? 1 : 0,
+        x: posts.length ? 0 : 20,
+        height: posts.length ? 80 : 0,
+        from: {opacity: 0, x: 20, height: 0},
+    });
+
+    const renderList = () => {
+        return (
+            trail.map(({ x, height, ...rest }, i) => {
+                return (
+                    <animated.div
+                        style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`) }}
+                        id={'feed'} key={posts[i].id}>
+                        <Post content={posts[i]}/>
+                    </animated.div>
+                )
+            })
+        )
     };
+
     if (posts.length) {
         return (
             <div id={'feed ml-0'}>
