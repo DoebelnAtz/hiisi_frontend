36eac9feafee8f38d7eff0a2b573ca1f7ea7b984
Anna Tuominen <atuomine@c2r3p8.hive.fi>
2020-02-21 11:46:56 +0200

    WIP: Started the design of profile page header

--- src/App/Profile/ProfilePage.js
+++ src/App/Profile/ProfilePage.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import AddFriend from "../Components/Buttons/AddFriend";
 
+const Filler = (props) => {
+    return <div className="filler" style={{ width: `${props.percentage}%` }} />
+}
+
+const ProgressBar = (props) => {
+    return (
+        <div className="progress_bar">
+        <Filler percentage={props.percentage} />
+        </div>
+    )
+}
+
+const ProfileHeader = (props) => {
+    const level = {
+        whole:Math.floor(props.level), 
+        next:Math.floor(props.level) + 1, 
+        percentage:((props.level - Math.floor(props.level)) * 100)
+    }
+
+    return (
+        <div className={'profile_header'}>
+        <img className={'profile_profile_pic'} src={props.profile_pic} alt={props.username}/>
+        <h2>{props.username}</h2>
+        <p>{props.active ? props.location : 'unavailable'}</p>
+        <ProgressBar {...level}/>
+    </div>
+    )
+}
+
 const ProfilePage = ({profile}) => {
     //TODO: add friend button
 
     return (
         <div id={'profile_page'} className={'container'}>
-            <div className={'row justify-content-center'}>
-                <img className={'profile_profile_pic'} src={profile.profile_pic} alt={profile.username}/>
-            </div>
+                <ProfileHeader {...profile}/>
             <div>
-                <p>username: {profile.username}</p>
                 <p>achievement points: {profile.achievement_points}</p>
                 <p>grade: {profile.grade}</p>
                 <p>class of: {profile.class_of}</p>
                 <p>evaluation points: {profile.correction_points}</p>
                 <p>coalition rank: {profile.coalition_rank}</p>
                 <p>level: {profile.level}</p>
-                <p>location: {profile.location}</p>
                 <p>coalition points: {profile.coalition_points}</p>
                 <p>wallet: {profile.wallet}</p>
                 <p>active: {profile.active ? 'active' : 'inactive'}</p>
