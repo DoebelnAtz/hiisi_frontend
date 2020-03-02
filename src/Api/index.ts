3dbe4720f2f6c10c7c1c78f89bdb15c54922c560
Doebelnatz <axel.ej.adlercreutz@gmail.com>
2020-03-02 16:52:56 +0200

    build update

--- src/Api/index.ts
+++ src/Api/index.ts
@@ -4,7 +4,7 @@ import { getLocal } from '../Utils';
 export const makeRequest = async (url: string, method: any, data: any = {}) => {
 	let resp;
 	resp = await axios({
-		url: `http://hivmeind-42.com/api/${url}`,
+		url: `http://hivemind-42.com/api/${url}`,
 		method: method,
 		data: data,
 		headers: {
