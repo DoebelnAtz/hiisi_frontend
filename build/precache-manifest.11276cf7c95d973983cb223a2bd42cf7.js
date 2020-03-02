590459f3de955d579a009fe4c395155468a0c4db
Doebelnatz <axel.ej.adlercreutz@gmail.com>
2020-03-02 15:56:59 +0200

    build version2

---
+++ build/precache-manifest.11276cf7c95d973983cb223a2bd42cf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58efe72430b1a3ae46d15f5f98281a4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6affeb3a50a4226e85c3",
+    "url": "/static/css/main.05a3e148.chunk.css"
+  },
+  {
+    "revision": "f6a21cdd651be24f2652",
+    "url": "/static/js/2.f85e0145.chunk.js"
+  },
+  {
+    "revision": "7ddd3150a226e6df4af7790f3b824358",
+    "url": "/static/js/2.f85e0145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6affeb3a50a4226e85c3",
+    "url": "/static/js/main.e4781bf8.chunk.js"
+  },
+  {
+    "revision": "f483d1f7cd20d8e3322f",
+    "url": "/static/js/runtime-main.70302f69.js"
+  },
+  {
+    "revision": "4f3a185e0d0d90af484cb96df3fb7234",
+    "url": "/static/media/giticon.4f3a185e.png"
+  }
+]);
