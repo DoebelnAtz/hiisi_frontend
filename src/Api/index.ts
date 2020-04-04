d32ac06a68d3fc2d828b1a3ff9cb6029a2226e7c
Doebelnatz <axel.ej.adlercreutz@gmail.com>
2020-04-04 16:25:37 +0300

    build update

--- src/Api/index.ts
+++ src/Api/index.ts
@@ -24,7 +24,6 @@ export const makeRequest = async (url: string, method: any, data: any = {}) => {
 	} catch (e) {
 		if (!e.response) {
 			//window.location.replace('/505');
-
 		}
 		else if (e.response.status === 401) {
 
